671bb114442f5035cb67d190bb92226b8d3904f8
JeonYooDeok <60500298+JeonYooDeok@users.noreply.github.com>
2021-09-17 16:10:19 +0900

    Add files via upload

---
+++ example.js
@@ -0,0 +1,14 @@
+const ethereumButton = document.querySelector('.connetMetamask');
+const myAccount = document.querySelector('.myAccount');
+
+ethereumButton.addEventListener('click', () => {
+    getAccount();
+});
+
+async function getAccount() {
+    const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+    });
+    const account = accounts[0];
+    myAccount.innerHTML = account;
+}
